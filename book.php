c535107161febc6b6b1a0876ebfc74b6a7c16bb9
ThanhBui <buivanthanh2727@gmail.com>
2018-05-29 16:14:50 +0700

    test

--- book.php
+++ book.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     include'config.php';
-    if (!isset($_SESSION['username'])) {
-        header('Location: index.php');
-    }    
+    // if (!isset($_SESSION['username'])) {
+    //     header('Location: index.php');
+    // }    
 ?>
 <!DOCTYPE html>
 <html lang="en">
